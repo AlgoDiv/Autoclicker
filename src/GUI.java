28a22d24fc58344bf0102cd77fad051342c4fffb
TimeChameleon7 <creepersuit7@gmail.com>
2019-04-22 17:06:44 -0500

    Got rid of text inside textfields

--- src/GUI.java
+++ src/GUI.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 public class GUI {
     private JRadioButton repeatRadioButton;
     private JRadioButton repeatForRadioButton;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField timesTextField;
+    private JTextField secondsTextField2;
     private JButton startButton;
     private JComboBox comboBox1;
     private JTextField secondsTextField;
