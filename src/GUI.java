56cc9e3c7276c71ef170062e32fe4fe0809161b8
Tyler Johnson <tylermackj@gmail.com>
2019-04-22 17:23:23 -0500

    Updated GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
     private JRadioButton repeatRadioButton;
     private JRadioButton repeatForRadioButton;
+    private boolean repeatBool;
+    private boolean repeatForBool;
     private JTextField textField1;
     private JTextField textField2;
     private JButton startButton;
@@ -10,9 +14,41 @@ public class GUI {
     private JTextField secondsTextField;
     private JTextField secondsTextField1;
     private JPanel panelMain;
+    private ButtonGroup group;
 
     public GUI() {
+        group = new ButtonGroup();
+        group.add(repeatRadioButton);
+        group.add(repeatForRadioButton);
+        repeatBool = false;
+        repeatForBool = false;
 
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double interval = Double.parseDouble(secondsTextField.getText());
+                    double delay = Double.parseDouble(secondsTextField1.getText());
+                }
+                catch(Exception exc) {
+                    JOptionPane.showMessageDialog(null,"Invalid values entered!", "Error", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+        repeatRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repeatBool = true;
+                repeatForBool = false;
+            }
+        });
+        repeatForRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repeatBool = false;
+                repeatForBool = true;
+            }
+        });
     }
 
     public static void main(String[] args) {
