427167258adba1eb6e0dd37708be7b9c3e4cefd9
TimeChameleon7 <creepersuit7@gmail.com>
2019-04-22 16:05:47 -0500

    Added GUI, no frame

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+    private JRadioButton repeatRadioButton;
+    private JRadioButton repeatForRadioButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton startButton;
+    private JComboBox comboBox1;
+    private JTextField secondsTextField;
+    private JTextField secondsTextField1;
+}
